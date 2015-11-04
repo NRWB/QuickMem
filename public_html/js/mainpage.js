b4bca2ed7f3bb7f463b5f711376b9e723f812229
Ankit Kumar <ankitkumar24@hotmail.com>
2015-11-03 18:09:28 -0800

    Subject, category selection now in one html page

--- public_html/js/mainpage.js
+++ public_html/js/mainpage.js
@@ -1,9 +1,28 @@
 var myFirebaseRef = new Firebase("https://quickmem.firebaseio.com/");
+var id = 1;
 myFirebaseRef.child("categories").on("child_added", function(snapshot) {
-	console.log(snapshot.val());  // Alerts "San Francisco"
+	var topic = snapshot.val();
 	var div = document.getElementById('buttonGroup');
 	var a = document.createElement('a');
+	a.id = "link-" + id;
+	id += 1;
 	a.className = "btn2 blue buttonsize";
-	a.innerHTML = snapshot.val();
+	a.onclick = function() { 
+		while (div.firstChild) {
+			div.removeChild(div.firstChild);
+		}
+		console.log(topic);
+		document.getElementById('title').innerHTML = topic;
+		var path = "subtopics/" + topic;
+		myFirebaseRef.child(path).on("child_added", function(snap) {
+			console.log(snap.val());
+			var subtopic = snap.val();
+			var a = document.createElement('a');
+			a.className = "btn2 blue buttonsize";
+			a.innerHTML = subtopic;
+			div.appendChild(a);
+		});
+	};
+	a.innerHTML = topic;
 	div.appendChild(a);
 });
